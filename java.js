dace107e42d9e5cf9890e8ac1a1a25992fd196ae
milad4d <154679120+milad4d@users.noreply.github.com>
2024-05-07 18:18:01 +0330

    Add files via upload

---
+++ java.js
@@ -0,0 +1,120 @@
+// document.querySelector(".box").addEventListener('click',function(){
+//     this.style.backgroundColor = 'red';
+// })
+
+let x='milad\'milad\'';
+console.log(x)
+
+let y = 'hello %s milad %d';
+console.log(y,'sssss',10)
+
+let z=`milslls
+'sasasas'
+${x}`;
+console.log(z)
+
+
+let output = `
+    <div class="alert">
+    ${x}
+
+    </div>
+`
+
+console.log(output.length)
+
+
+let fname='milad';
+console.log(fname.charAt(2))
+
+console.log(fname.slice(2))
+
+let lname ='takjoo';
+
+console.log(fname.concat(lname,'nnn'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
