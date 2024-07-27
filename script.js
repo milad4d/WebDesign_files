76eac4f0d057dcec7b86b75c56167c26a6366e93
Milad Takjoo <154679120+milad4d@users.noreply.github.com>
2024-07-27 18:29:03 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-const car={
-    model:'',
-    wheels:0,
-    width:0,
-    lenght:0,
-    engine:{
-        status:false,
-    },
-    start:function(){
-        this.engine=true;
-    },
-    stop:function(){
-        this.engine=false;
-    },
-
-    doors:{
-        1:{
-            lock:false,
-        },
-    },
-    2:{
-        lock:true,
-    },
-    // اگر گفت کدام در آن را باز کن...اگر نگفت همه را باز کن
-    lock:function(doorNumber=null){
-        if (doorNumber==null){
-            for(let i=1;i<=this.doors.lenght;i++){
-                this.doors[i].lock=true
-            }
-            return;
+$('#resp').click(function(){
+    
+    $('div').toggleClass(function(){
+        if($(this).hasClass('show')){
+            $(this).fade
+            return '';
         }
-        this.doors[doorNumber].lock=true;
-    },
+        return 'show'
+    });
+    $('nav').toggleClass('respHeight')
+})
 
-    unlock:function(doorNumber=null){
-        if (doorNumber==null){
-            for(let i=1;i<=this.doors.lenght;i++){
-                this.doors[i].lock=false
-            }
-            return;
-        }
-        this.doors[doorNumber].lock=false;
-    },
-
-};
-car.lock(2)
